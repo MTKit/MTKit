70ece323f38cc6154506eb29a808dc037171f4aa
bill19 <sunhao.private@foxmail.com>
2017-09-25 00:16:38 -0500

    addkit

---
+++ MTKIT/MTKIT/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MTKIT
+//
+//  Created by HaoSun on 2017/9/25.
+//  Copyright © 2017年 MTKIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
